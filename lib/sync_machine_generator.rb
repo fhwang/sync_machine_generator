6e9a52eaaf91aaa9e39da892e21a7542d14c4088
Francis Hwang <sera@fhwang.net>
2019-10-09 12:47:14 -0400

    Initial commit.

---
+++ lib/sync_machine_generator.rb
@@ -0,0 +1,6 @@
+require "sync_machine_generator/version"
+
+module SyncMachineGenerator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/sync_machine_generator/version.rb
@@ -0,0 +1,3 @@
+module SyncMachineGenerator
+  VERSION = "0.1.0"
+end
